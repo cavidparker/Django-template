8f0b6e519f12211a67cc417700b942fcafb09f10
Tariqul Islam <cavidpark@gmail.com>
2021-01-27 16:05:36 +0600

    first commit

---
+++ pagination_class_based/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PaginationClassBasedConfig(AppConfig):
+    name = 'pagination_class_based'
---
+++ pagination_func_based/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PaginationFuncBasedConfig(AppConfig):
+    name = 'pagination_func_based'
